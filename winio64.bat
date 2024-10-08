0acf2f8497ca89360cb8291d8f7743667af49bb1
Jazzi <123866789+JazziDev@users.noreply.github.com>
2024-10-08 14:32:48 +0100

    main bat

---
+++ winio64.bat
@@ -0,0 +1,57 @@
+@echo off
+
+:: BatchGotAdmin
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+@echo off
+
+cls
+setlocal
+call :setESC 
+echo %ESC%[94m  - MADE - BY - VALVONTISH -%ESC%[0m
+echo.
+echo %ESC%[94m  - https://solo.to/valvontish -%ESC%[0m
+echo.
+pause
+
+start cmd /k "sc stop winio64"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)
